856f0cfe3151c16d4951aabf658981f91fc43db7
Ruggy Mango <rans2203@gmail.com>
2024-05-30 15:02:15 +0530

    make necessary changes

--- next-project/next.config.mjs
+++ next-project/next.config.mjs
@@ -1,20 +1,20 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-// export default nextConfig;
+export default nextConfig;
 
 
-import path from 'path';
+// import path from 'path';
 
-export default {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      };
-    }
-    config.resolve.alias['@'] = path.resolve(process.cwd());
+// export default {
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       config.node = {
+//         fs: 'empty'
+//       };
+//     }
+//     config.resolve.alias['@'] = path.resolve(process.cwd());
 
-    return config;
-  },
-};
+//     return config;
+//   },
+// };
