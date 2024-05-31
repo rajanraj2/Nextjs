f547fbf94b19e90e88334200df2d12934d3788ba
Ruggy Mango <rans2203@gmail.com>
2024-05-31 20:11:52 +0530

    Add react-project2

---
+++ react-project2/client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      Hello from app
+      <Button color="primary">Hello World</Button>;
+    </div>
+    </>
+  );
+}
+
+export default App;
